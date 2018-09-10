87f688db1ab724046880e552338d547c9b75b3f0
Lorraine Phillips <lorraine.phillips@softwire.com>
2018-09-10 13:44:50 +0100

    Implemented Asp.Net Web Framework

---
+++ Bookish.ConsoleApp/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookish.ConsoleApp
+{
+    class Item
+    {
+        public int ItemID { get; set; }
+        public int UserID { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public int BookID { get; set; }
+    }
+}
--- Bookish.ConsoleApp/Program.cs
+++ Bookish.ConsoleApp/Program.cs
@@ -3,13 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using Dapper;
+using System.Collections.Specialized;
+
 
 namespace Bookish.ConsoleApp
 {
+    class Book
+    {
+        public int BookID { get; set; }
+        public char ISBN { get; set; }
+        public char BookTitle { get; set; }
+        public char BookAuthor { get; set; }
+    }
+    class Item
+    {
+        public int ItemID { get; set; }
+        public int UserID { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public int BookID { get; set; }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            string SqlString = "SELECT TOP 100 [ItemID],[UserID],[ReturnDate],[BookID] FROM [Items]";
+            var ourItems = (List<Item>)db.Query<Item>(SqlString);
+            foreach (var Item in ourItems)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\nItem ID: " + Item.ItemID.ToString());
+                Console.WriteLine("\nUser ID: " + Item.UserID.ToString());
+                Console.WriteLine("\nReturn Date: " + Item.ReturnDate.ToString());
+                Console.WriteLine("\nBook ID: " + Item.BookID.ToString());
+                Console.WriteLine(new string('*', 20));
+            }
+
+            Console.ReadLine();
         }
     }
 }
